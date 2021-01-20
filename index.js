51c63f9fc09b9b69041020cfc364db670ff2acd2
Matt Slack <slack@integernoun.com>
2021-01-20 13:30:35 -0500

    Copy Benefit's .stylelint.rc into index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  plugins: [
+    'stylelint-order',
+    'stylelint-scss'
+  ],
+  rules: {
+    'at-rule-no-unknown': null,
+    'color-named': 'never',
+    'declaration-property-unit-disallowed-list': {
+      'font-size': ['vw', 'vh', 's']
+    },
+    'no-descending-specificity': [
+      true, {
+        ignore: ['selectors-within-list'],
+        severity: 'warning'
+      }
+    ],
+    'number-leading-zero': 'always',
+    'order/order': [
+      'custom-properties',
+      'declarations'
+    ],
+    'order/properties-alphabetical-order': true,
+    'scss/at-rule-no-unknown': true
+  }
+}
